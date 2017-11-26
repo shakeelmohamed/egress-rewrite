36307eb9ef271915d6caecf5731c71eded86ae0e
Shakeel Mohamed <contact@shakeel.xyz>
2017-11-26 01:30:33 -0800

    Add Sentry logging, tests+coverage; restructure app

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const dotenv = require("dotenv");
-const server = require("./server");
-
-dotenv.config();
-
-const port = process.env.PORT || 5000;
-server.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(`Listening on port ${port}.`);
-});
+module.exports = require("./lib/egress");
