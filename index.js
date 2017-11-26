e48bfbdd57883c5888105da4de98aa7f8bb09b33
Shakeel Mohamed <contact@shakeel.xyz>
2017-11-25 23:47:23 -0800

    nits

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const dotenv = require("dotenv");
-
 const server = require("./server");
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-server.listen(port, (err) =>  {
+server.listen(port, (err) => {
     if (err) {
         console.log(err);
         return;
